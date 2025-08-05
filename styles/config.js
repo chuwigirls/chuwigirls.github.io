76cd250b8e54dd7cb3a2aeef784366efa3e43718
chuwigirls <112835417+chuwigirls@users.noreply.github.com>
2025-08-05 19:38:29 -0400

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -1,13 +1,24 @@
+/* 
+        _..---.--.
+       .'\ __|/O.__)
+      /__.' _/ .-'_\
+     (____.'.-_\____)
+      (_/ _)__(_ \_)\_
+    mrf(_..)--(.._)'--'
+
+    if you're looking at this page to learn about coding,
+    ask chuwigirls for help! */
+
 // ==============================
 // =========== Sheets ===========
 // ==============================
 const sheetConfigs = {
   narapedia: {
     id: "1lGc4CVqcFr9LtcyVW-78N5En7_imdfC8bTf6PRUD-Ms",
-    sheetName: "Masterlist" // Change to your actual sheet/tab name
+    sheetName: "Masterlist"
   },
   naratrove: {
     id: "1FGsqhNZ_fYW-njhJlP39r-nQeK2X4FCIXeC_FTeU6lM",
-    sheetName: "Inventory" // Change to your actual sheet/tab name
+    sheetName: "Inventory"
   }
 };
