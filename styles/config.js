ea011fe8890a0dbdbe55a6730434d13b523dd1d4
chuwigirls <112835417+chuwigirls@users.noreply.github.com>
2025-08-07 19:32:50 -0400

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -15,7 +15,37 @@
 const sheetConfigs = {
   narapedia: {
     id: "1lGc4CVqcFr9LtcyVW-78N5En7_imdfC8bTf6PRUD-Ms",
+    sheetName: "Masterlist"a/* 
+        _..---.--.
+       .'\ __|/O.__)
+      /__.' _/ .-'_\
+     (____.'.-_\____)
+      (_/ _)__(_ \_)\_
+    mrf(_..)--(.._)'--'
+
+    if you're looking at this page to learn about coding,
+    ask chuwigirls for help! */
+
+// ==============================
+// =========== Sheets ===========
+// ==============================
+const sheetConfigs = {
+  narapedia: {
+    id: "1lGc4CVqcFr9LtcyVW-78N5En7_imdfC8bTf6PRUD-Ms",
     sheetName: "Masterlist"
+  },
+  naratrove: {
+    id: "1FGsqhNZ_fYW-njhJlP39r-nQeK2X4FCIXeC_FTeU6lM",
+    sheetName: "Inventory"
+  }
+};
+
+// ==============================
+// ======== Discord OAuth =======
+// ==============================
+const discordClientId = '1319474218550689863';
+const redirectUri = 'https://chuwigirls.github.io/user.html';
+const discordScopes = 'identify';
   },
   naratrove: {
     id: "1FGsqhNZ_fYW-njhJlP39r-nQeK2X4FCIXeC_FTeU6lM",
